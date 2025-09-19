815f0241fd205cb24e7b18f23bafcbe9c28bf3b1
AM-MUGAMBI <amugambi370@gmail.com>
2025-09-19 12:21:10 +0300

    ur

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers  # Import routers from rest_framework
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()  # explicitly use DefaultRouter()
+router = routers.DefaultRouter()  # Create a DefaultRouter instance
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
