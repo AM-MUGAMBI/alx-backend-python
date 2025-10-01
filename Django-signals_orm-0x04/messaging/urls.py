267f6ed8112015b0857be759e9aeac04ad451a87
AM-MUGAMBI <amugambi370@gmail.com>
2025-10-01 19:08:17 +0300

    ur

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import delete_user
+from .views import delete_user, home
 
 urlpatterns = [
+    path('', home, name='home'),  # This handles "/"
     path('delete_user/', delete_user, name='delete_user'),
 ]
-
